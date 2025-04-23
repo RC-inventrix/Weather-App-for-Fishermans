1863670d69128b24eff1e0cea10bd9f15007c813
RCN <ravindunishshankawork@gmail.com>
2025-04-23 22:42:46 +0530

    Updated the .gitignore
    .idea/

--- src/main/java/weather/app/IntegrationTest.java
+++ src/main/java/weather/app/IntegrationTest.java
@@ -3,8 +3,8 @@ package weather.app;
 public class IntegrationTest {
     public static void main(String[] args) {
         Inputs inputData = new Inputs();
-        inputData.setLocation("Galle");
-        inputData.setDateTime("2025-04-21 18:00:00");
+        inputData.setLocation("Matara");
+        inputData.setDateTime("2025-04-23 09:00:00");
         inputData.setNoOfCrewMembers(5);
         inputData.setMaxDays(2);
 
