c4a81f0437a2b0f6743481c4177a81be46bf328e
RCN <ravindunishshankawork@gmail.com>
2025-05-30 13:21:11 +0530

    GUI Working
    GUI is working but has to make it better

--- src/main/java/weather/app/MainApp.java
+++ src/main/java/weather/app/MainApp.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("interface.fxml")); // your FXML file
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/interface.fxml"))); // your FXML file
         primaryStage.setTitle("Weather Safety Advisor");
         primaryStage.setScene(new Scene(root, 600, 400)); // set size according to your layout
         primaryStage.show();
